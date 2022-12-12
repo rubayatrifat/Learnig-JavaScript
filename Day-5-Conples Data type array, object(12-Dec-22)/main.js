fe4f561d9a9012ba8abd819643ccca9393273794
rubayatrifat <rubayatislam17@gmail.com>
2022-12-12 12:18:09 +0600

    Array A

---
+++ Day-5-Conples Data type array, object(12-Dec-22)/main.js
@@ -0,0 +1,34 @@
+// *** Array ***
+
+let manTotho = ['Rifat', 'Islam', 12, 'Web Devoloper']
+
+console.log(manTotho[1])
+
+console.log(manTotho[0] + " " + manTotho[1]);
+
+manTotho[4] = "Rubayat";
+
+manTotho[0] = 'Rubayat'
+
+console.log(manTotho[0] + " " + manTotho[1]);
+
+console.log(manTotho)
+
+
+let lasteleAray = manTotho.length - 1
+
+console.log(manTotho[lasteleAray])
+
+// *** Object ***
+
+let firstObj = {
+    firstName : 'Rifat',
+    lastName : 'Islam',
+    age : 12
+}
+console.log(firstObj.firstName);
+console.log(firstObj.firstName + ' ' + firstObj.lastName + ' is ' + firstObj.age )
+
+// *** Templating String ***
+
+console.log(`${firstObj.firstName} ${firstObj.lastName}`)
