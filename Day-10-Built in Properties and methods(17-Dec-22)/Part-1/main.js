da7a609b317e1715636bc43a51c371c5a1594116
rubayatrifat <rubayatislam17@gmail.com>
2022-12-18 13:13:39 +0600

    JS Loop

---
+++ Day-10-Built in Properties and methods(17-Dec-22)/Part-1/main.js
@@ -0,0 +1,17 @@
+// Javascript Built in properties
+
+const str = ' Hello World '
+
+console.log(str.length)
+console.log(str.toUpperCase())
+console.log(str.toLowerCase())
+console.log(str.indexOf('l'))
+console.log(str.lastIndexOf('l'))
+console.log(str.includes('Hell'))
+console.log(str.startsWith('World '))
+console.log(str.endsWith('World '))
+
+
+const trim = str.length
+console.log(trim)
+// trimStart and trimEnd
--- Day-9-Loop(16-Dec-22)/Part-2/exercise.js
+++ Day-9-Loop(16-Dec-22)/Part-2/exercise.js
@@ -3,15 +3,26 @@
 Total Number-20
 Try to use function as required
 
-# Exercise-1(what will be the output and explain)-2
+# Exercise-1 -2
+
+let num = 1;
+num = 3
+
+now white a while lopp.
+and what is it output ?
+
+*/
 
 let num = 1;
 
 while(num <= 10) {
-console.log(num);
-num += 2;
+    console.log(num);
+    num += 2;
 }
 
+
+/*
+ 
 # Exercise-2(what will be the output and explain)-2
 
 let num = 1
@@ -23,6 +34,20 @@ console.log(num);
 num++;
 }
 
+*/
+
+let num2 = 1;
+
+while (num2 <= 20) {
+  if (num2 % 4 === 0) {
+    console.log(num2);
+  }
+  num++;
+}
+//explain: let num2 = 1; while er condetion e dawa ase num jotokkhon 20 er soman ken ba bis theke sorboccho vabe choto hobe totokhon loop ti cholbe tarpor if use kora hoyese abong 20 ke  4 dara vag kore vag sesber kora hoise tai er vagses hobe 0 abog 0 is === than 0 tai output hobe 0
+
+/*
+
 # Exercise-3(what will be the output and why?)-2
 
 let num = 100;
@@ -31,62 +56,14 @@ console.log(num + 1);
 num--;
 }
 
-# Exercise-4(what will be the output? it May require if condition inside loop)-4
+*/
 
-Print out all even numbers between 1 and 100. Write two solutions: one with a while loop and one with a for loop.
+// Ans of exer 3: It Out put will be a infinite loop. It's very dangeres loop. So, we do not use this loop
 
-# Exercise-5(Run loop and add $ in each repetition)-4
+/*
 
-function repeat(num){
-//code
-}
 
-repeat(3)
-
-<!--
-$
-$$
-$$$ -->
-
-repeat(5)
-
-<!--
-$
-$$
-$$$
-$$$$
-$$$$$ -->
-
-# Exercise-6(what will be the output and Let's explain) -3
-
-Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
-
-function fizzBuzz(num){
-//code
-}
-
-fizzBuzz(100)
-
-<!--
-1
-2
-Fizz
-4
-Buzz
-Fizz
-7
-8
-Fizz
-Buzz
-11
-Fizz
-13
-14
-FizzBuzz
-...
-... -->
-
-Exercise-7:Write a function that takes two strings (a and b) as arguments
+Exercise-4:Write a function that takes two strings (a and b) as arguments
 Return the number of times a occurs in b - 3
 function myFunction(a, b) {
 return
@@ -98,3 +75,13 @@ myFunction('z', 'how many times does the character occur in this se
 
 
 */
+
+function myFunction(a, b){
+    console.log(`${a} ${b}`)
+    return
+}
+
+myFunction('m', 'how many times does the character occur in this sentence?') 
+myFunction('h', 'how many times does the character occur in this sentence?') 
+myFunction('?', 'how many times does the character occur in this sentence?') 
+myFunction('z', 'how many times does the character occur in this sentence?')
