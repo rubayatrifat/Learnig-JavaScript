db4341972b7cb5015a2371408d371002c30356ba
rubayatriat <rubayatislam17@gmail.com>
2023-01-20 22:55:43 +0600

    JSON API Ajax

---
+++ Day-28-JSON API Ajax(19-1-23)/main.js
@@ -0,0 +1,28 @@
+// JSON (Java Script Object Notation) - Data exchange format
+
+// JS regular object
+const myObj = {
+    name: 'Rifat',
+    age: 13,
+    cell: '01912819245',
+    isDevoper: true,
+    fullName() {
+        return this.name
+    }
+}
+
+// JSON Object
+
+const myJsonObj = {
+    "name": "Rifat",
+    "age": 13,
+    "cell": "01912819245",
+    "isDevoper": true
+}
+
+
+
+
+
+
+
