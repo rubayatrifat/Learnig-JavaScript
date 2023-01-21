d1e1b754287544364fcae4dec2ba216f1bb8a7f5
rubayatriat <rubayatislam17@gmail.com>
2023-01-21 20:47:01 +0600

    JSON API Ajax

--- Day-28-JSON API Ajax(19-1-23)/main.js
+++ Day-28-JSON API Ajax(19-1-23)/main.js
@@ -1,28 +1,57 @@
 // JSON (Java Script Object Notation) - Data exchange format
 
 // JS regular object
-const myObj = {
-    name: 'Rifat',
-    age: 13,
-    cell: '01912819245',
-    isDevoper: true,
-    fullName() {
-        return this.name
-    }
-}
+// const myObj = {
+//     name: 'Rifat',
+//     age: 13,
+//     cell: '01912819245',
+//     isDevoper: true,
+//     fullName() {
+//         return this.name
+//     }
+// }
 
 // JSON Object
 
-const myJsonObj = {
-    "name": "Rifat",
-    "age": 13,
-    "cell": "01912819245",
-    "isDevoper": true
+// const myJsonObj = {
+//     "name": "Rifat",
+//     "age": 13,
+//     "cell": "01912819245",
+//     "isDevoper": true
+// }
+
+const btnElm = document.querySelector('button')
+const paraElm = document.querySelector('p')
+
+
+
+function loadRandomJock() {
+
+    fetch('https://api.chucknorris.io/jokes/random')
+    .then((response) => response.json())
+    .then((data) => {
+        const category = data[3]
+        return fetch(
+            `https://api.chucknorris.io/jokes/random?category=${category}`
+        )
+    })
+    .then((response) => response.json()) 
+    .then((data) => {
+        paraElm.textContent = data.value
+    }) 
+    .catch((err) => console.log(err))
+
+    // async function findJock() {
+    //     try {
+    //         const response = await fetch('https://api.chucknorris.io/jokes/random')
+    //         const data = await response.json()
+    //         console.log(data)   
+    //     }catch (err){
+    //         console.log(err)
+    //     }
+
+    // }
+    
 }
 
-
-
-
-
-
-
+btnElm.addEventListener('click', loadRandomJock)
