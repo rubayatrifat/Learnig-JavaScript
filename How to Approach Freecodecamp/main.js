f1eba19c5a789094428e4fb21b95464dc9834255
rubayatriat <rubayatislam17@gmail.com>
2023-01-30 19:08:06 +0600

    How to Approach Freecodecamp

--- How to Approach Freecodecamp/main.js
+++ How to Approach Freecodecamp/main.js
@@ -7,6 +7,7 @@ function printday(dayNum) {
 console.log(printday(6))
 
 
+// Last item
 function lastElm(arr) {
     const lastElmIndex = arr.length - 1
     console.log(arr[lastElmIndex])
@@ -15,3 +16,16 @@ function lastElm(arr) {
 lastElm([1, 93, 8743, 8794, 957, 8906, 89067, 849])
 lastElm(['Apple', 'Orange', 'Grape', 'Papaiya', 'Mango', 'Jackfruit'])
 lastElm(['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Satarday'])
+
+// create obj
+function createObj(val1, val2, val3) {
+    obj = {
+        x: val1,
+        y: val2,
+        z: val3
+    }
+
+    console.log(obj)
+}
+
+createObj(3, 6, 4)
